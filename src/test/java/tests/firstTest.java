271f890ab4c19cc65666e6a558949114bd958819
rmznsnr27 <ramazan.sener6507@gmail.com>
2022-01-15 23:25:11 +0300

    web elements work and operations

--- src/test/java/tests/firstTest.java
+++ src/test/java/tests/firstTest.java
@@ -1,6 +1,9 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -14,7 +17,32 @@ public class firstTest {
 //         for the firefox laucnh
 //        WebDriver driver= new FirefoxDriver();
 
-        driver.get("https://www.hepsiburada.com/");
+        driver.get("https://demoqa.com/text-box");
+
+        driver.manage().window().maximize();
+
+        WebElement fullName= driver.findElement(By.id("userName"));
+        fullName.click();
+        fullName.sendKeys("HUSOAGA");
+
+        WebElement email = driver.findElement(new By.ByCssSelector(".mr-sm-2[id='userEmail']"));
+        email.click();
+        email.sendKeys("ramazan@gmail.com");
+
+        WebElement address = driver.findElement(new By.ByCssSelector(".form-control[placeHolder='Current Address']"));
+        address.click();
+        address.sendKeys("İstasyon, Atatürk Cd., 46800 Türkoğlu/Kahramanmaraş");
+
+        WebElement permanentAddress= driver.findElement(By.id("permanentAddress"));
+        permanentAddress.click();
+        permanentAddress.sendKeys("İstasyon, Atatürk Cd., 46800 Türkoğlu/Kahramanmaraş");
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,250)", "");
+        
+        WebElement submitButton= driver.findElement(By.id("submit"));
+        submitButton.click();
+
 
     }
 }
